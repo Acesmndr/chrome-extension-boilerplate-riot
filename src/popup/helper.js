64099131bcf7f6c0242104f7d026b47f4d1c2b9d
acesmndr <acesmndr@gmail.com>
2019-09-28 19:19:01 +0545

    remove helper

--- src/popup/helper.js
+++
@@ -1,8 +0,0 @@
-import * as riot from 'riot';
-
-export default {
-  route(page) {
-    riot.unmount(document.getElementById('root'), true);
-    riot.mount(document.getElementById('root'), {}, page);
-  }
-}
